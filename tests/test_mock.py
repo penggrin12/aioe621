ce51143c44a0a4e42b6cdcc19eb47e54280dda86
penggrin12 <miner.sidor@gmail.com>
2026-07-15 22:33:17 +0300

    test posts.list & posts.random

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -11,6 +11,7 @@ from aioe621.schemas.posts import Post, PostRating
 from aioe621.schemas.tags import RelatedTag, Tag
 
 MOCK_POST_JSON = '{"id":6543578,"created_at":"2026-07-13T17:36:26.837+03:00","updated_at":"2026-07-15T02:29:27.631+03:00","change_seq":78095660,"files":{"meta":{"md5":"077e4bbffe0ea5d5acb0fca479de8fdd","ext":"png","size":36622,"duration":null,"has_sample":true},"original":{"width":1745,"height":1698,"url":"https://static1.e621.net/data/07/7e/077e4bbffe0ea5d5acb0fca479de8fdd.png"},"preview":{"width":263,"height":256,"jpg":"https://static1.e621.net/data/preview/07/7e/077e4bbffe0ea5d5acb0fca479de8fdd.jpg","webp":"https://static1.e621.net/data/preview/07/7e/077e4bbffe0ea5d5acb0fca479de8fdd.webp"},"sample":{"width":874,"height":850,"jpg":"https://static1.e621.net/data/sample/07/7e/077e4bbffe0ea5d5acb0fca479de8fdd.jpg","webp":"https://static1.e621.net/data/sample/07/7e/077e4bbffe0ea5d5acb0fca479de8fdd.webp"}},"uploader_id":94865,"uploader_name":"Strikerman","approver_id":null,"stats":{"score":{"up":35,"down":-1,"total":34},"fav_count":36,"is_favorited":false,"vote":0,"comment_count":1,"hotness":4131.053207706494},"flags":{"pending":false,"flagged":false,"note_locked":false,"status_locked":false,"rating_locked":false,"deleted":false},"has":{"parent":true,"children":false,"active_children":false,"notes":false,"sample":true},"relationships":{"parent_id":6543578,"children":[]},"pools":[],"rating":"s","locked_tags":[],"sources":["https://x.com/DalueArt/status/2076512463059894388","https://pbs.twimg.com/media/HNFBBh6aUAA4DA5?format=png&name=orig"],"description":"The secret 3rd option","tags":{"general":["anthro","anthro_on_anthro","bedroom_eyes","black_collar","black_eyes","black_nose","blue_eyes","blush","bodily_fluids","collar","duo","emanata","exposed_shoulder","eye_contact","floppy_ears","grin","half-closed_eyes","hand_on_chest","heart_symbol","intraspecies","looking_at_another","male","male/male","narrowed_eyes","nervous","nervous_smile","pupils","seductive","selfcest","side_view","simple_background","smile","square_crossover","surprised","sweat","white_background"],"artist":["dalueart"],"contributor":[],"copyright":[],"character":["dalue_(dalueart)"],"species":["canid","canine","canis","domestic_dog","mammal"],"invalid":[],"meta":["hi_res"],"lore":[]}}'
+MOCK_POSTS_JSON = f"[{MOCK_POST_JSON}]"
 MOCK_TAGS_JSON = '[{"id":1068,"name":"canine","post_count":1611254,"related_tags":"canid 300 canine 300 mammal 300 anthro 257 hi_res 208 male 206 fur 175 canis 174 genitals 168 female 160 clothing 134 solo 127 duo 126 hair 119 penis 113 tail 110 breasts 107 bodily_fluids 105 nude 95 text 95 digital_media_(artwork) 93 nipples 92 wolf 91 fox 89 balls 85","related_tags_updated_at":"2026-07-08T07:00:44.477-04:00","category":5,"is_locked":false,"created_at":"2020-03-05T05:49:37.994-05:00","updated_at":"2026-07-08T07:00:44.477-04:00"}]'
 
 MOCK_404_JSON = '{"success":false,"reason":"not found"}'
@@ -43,17 +44,21 @@ class MyTestCase(unittest.IsolatedAsyncioTestCase):
     # posts
 
     @respx.mock
-    async def test_posts_get(self) -> None:
-        route = respx.get(f"{self.client.E621_BASE_URL}/posts/6543578.json").mock(
-            return_value=httpx.Response(200, text=MOCK_POST_JSON)
+    async def test_posts_list(self) -> None:
+        route = respx.get(f"{self.client.E621_BASE_URL}/posts.json").mock(
+            return_value=httpx.Response(200, text=MOCK_POSTS_JSON)
         )
 
-        post = await self.client.posts.get(id=6543578)
+        posts = await self.client.posts.list(tags="nervous", limit=1)
 
         self.assertTrue(route.called)
         request = route.calls.last.request
         self.assertEqual(request.headers.get("User-Agent"), self.client.user_agent)
 
+        self.assertIsInstance(posts, tuple)
+        self.assertEqual(len(posts), 1)
+
+        post = posts[0]
         self.assertIsInstance(post, Post)
         self.assertEqual(post.id, 6543578)
         self.assertEqual(post.files.meta.ext, "png")
@@ -63,6 +68,26 @@ class MyTestCase(unittest.IsolatedAsyncioTestCase):
         self.assertIn("dalueart", post.tags.artist)
         self.assertEqual(post.description, "The secret 3rd option")
         self.assertEqual(post.rating, PostRating.SAFE)
+
+    @respx.mock
+    async def test_posts_get(self) -> None:
+        respx.get(f"{self.client.E621_BASE_URL}/posts/6543578.json").mock(
+            return_value=httpx.Response(200, text=MOCK_POST_JSON)
+        )
+        post = await self.client.posts.get(id=6543578)
+
+        self.assertIsInstance(post, Post)
+        self.assertEqual(post.id, 6543578)
+
+    @respx.mock
+    async def test_posts_random(self) -> None:
+        respx.get(f"{self.client.E621_BASE_URL}/posts/random.json").mock(
+            return_value=httpx.Response(200, text=MOCK_POST_JSON)
+        )
+        post = await self.client.posts.random()
+
+        self.assertIsInstance(post, Post)
+        self.assertEqual(post.id, 6543578)
 
     @respx.mock
     async def test_posts_get_immutability(self) -> None:
